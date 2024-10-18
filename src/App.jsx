c3fdf83acc0abb4bd3fe094dfbc8e19c33919632
anhthu <anhthucute5124@gmail.com>
2024-10-18 19:41:28 +0700

    trang liên hệ

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "notyf/notyf.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "./pages/Home/Index";
+import Contact from "./pages/Home/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact-us.html" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
