2012d5f512b8beea188f9c00fba859d15515443c
ThuHocVietCode <thuptm1205@gmail.com>
2024-10-26 17:32:41 +0700

    updated bug

--- src/pages/Service/Index.jsx
+++ src/pages/Service/Index.jsx
@@ -10,32 +10,28 @@ function Index() {
     {
       name: "Uốn sóng lơi ",
       slug: "Uon-song-loi",
-      image:
-        "https://i.pinimg.com/564x/a7/3f/d6/a73fd6d184f46949fa3c8966280e79fb.jpg",
+      image: "https://i.pinimg.com/564x/a7/3f/d6/a73fd6d184f46949fa3c8966280e79fb.jpg",
       price: 500000,
       discount: 400000,
     },
     {
       name: "Duỗi phục hồi",
       slug: "duoi-phuc-hoi",
-      image:
-        "https://i.pinimg.com/enabled_hi/564x/44/04/c0/4404c0426518962249ba69e3716dc9b1.jpg",
+      image: "https://i.pinimg.com/enabled_hi/564x/44/04/c0/4404c0426518962249ba69e3716dc9b1.jpg",
       price: 1000000,
       discount: 800000,
     },
     {
       name: "Nhuộm tóc ",
       slug: "nhuom-toc",
-      image:
-        "https://i.pinimg.com/enabled_hi/564x/b9/9c/9f/b99c9f1aaf7284a62c7622af9a5b9684.jpg",
+      image: "https://i.pinimg.com/enabled_hi/564x/b9/9c/9f/b99c9f1aaf7284a62c7622af9a5b9684.jpg",
       price: 750000,
       discount: 600000,
     },
     {
       name: "Tạo kiểu tóc nam",
       slug: "tao-kieu-toc-nam",
-      image:
-        "https://i.pinimg.com/736x/00/ee/05/00ee05f3696c3f74f5a598df6ab10e85.jpg",
+      image: "https://i.pinimg.com/736x/00/ee/05/00ee05f3696c3f74f5a598df6ab10e85.jpg",
       price: 300000,
       discount: 240000,
     },
@@ -44,16 +40,10 @@ function Index() {
     <>
       <Header />
       <BreadcrumbComponent props={[{ name: "Dịch vụ", url: "/dich-vu" }]} children={null} />
-      <Container className='my-3'>
-        <div className='text-start border-0 rounded-0 border-start border-primary border-5 h-100 mb-3'>
-          <div className='ms-2'>
-            <h3 className='mb-0 h3 fw-bold text-uppercase text-primary-emphasis'>Dịch vụ</h3>
       <Container className="my-5">
         <div className="text-start border-0 rounded-0 border-start border-primary border-5 h-100 mb-3">
           <div className="ms-2">
-            <h3 className="mb-0 h3 fw-bold text-uppercase text-primary-emphasis">
-              Dịch vụ
-            </h3>
+            <h3 className="mb-0 h3 fw-bold text-uppercase text-primary-emphasis">Dịch vụ</h3>
           </div>
         </div>
         <Row className="row-cols-1 row-cols-lg-4 g-4">
@@ -72,9 +62,7 @@ function Index() {
                   <i className="bi bi-truck" />
                 </div>
                 <h5 className="fw-bold">Giao hàng siêu tốc 2h</h5>
-                <p className="mb-0">
-                  Nhận hàng ngay trong 2 giờ! Nhanh chóng, tiện lợi.
-                </p>
+                <p className="mb-0">Nhận hàng ngay trong 2 giờ! Nhanh chóng, tiện lợi.</p>
               </Card.Body>
             </Card>
           </Col>
